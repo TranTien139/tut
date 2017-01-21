73ab9f87e21b0c96aea77e3d335929d490695fe4
TranTien139 <tienfethust@gmail.com>
2017-01-21 11:57:37 +0700

    commit

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1809357332636523', // your App ID
+        'clientSecret'  : '854746d2d2a894ee0d579c251e749d2f', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
